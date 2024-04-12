5814df093c35b4d5b18ad9a5c530c6c64c2c737b
Lataretu, Marie <LataretuM@rki.de>
2024-04-12 16:48:19 +0200

    linting changes

--- bin/clean_segment_DB.R
+++ bin/clean_segment_DB.R
@@ -18,13 +18,13 @@ segment <- fread(file = stats, header = FALSE, sep = "\t", fill = TRUE)
 # extract only the sequences that are not to short/long
 segment_good_len <- segment[which(segment$V2 >= (median(segment$V2) - 200) & segment$V2 <= (median(segment$V2) + 200)), ]
 # extract only sequences with less than 5% ambiguous bases
-segment_good_len_fewAmbig <- segment_good_len[which(segment_good_len$V5 <= 5), ]
+segment_good_len_few_ambig <- segment_good_len[which(segment_good_len$V5 <= 5), ]
 # remove sequences with wrong characters
-segment_good_len_fewAmbig_goodqual <- segment_good_len_fewAmbig[which(segment_good_len_fewAmbig$V4 <= 0), ]
+segment_good_len_few_ambig_goodqual <- segment_good_len_few_ambig[which(segment_good_len_few_ambig$V4 <= 0), ]
 
 # read in coresponding fasta file
 fastain <- read.fasta(fasta)
-fastaout <- fastain[c(which(names(fastain) %in% segment_good_len_fewAmbig_goodqual$V1))]
+fastaout <- fastain[c(which(names(fastain) %in% segment_good_len_few_ambig_goodqual$V1))]
 
 # export short fasta
 write.fasta(fastaout, names = names(fastaout), file.out = paste0(name, "_fewAmbig-corLen.fasta"))
